ba60345c826652a3a4b823feb4010930c9ff7fcf
John Corrales <jjcorrales3@gmail.com>
2020-02-02 16:15:40 -0800

    call the command from the when

--- init_test.go
+++ init_test.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"github.com/DATA-DOG/godog"
 	"llama/cmd"
+	//"os"
 )
 
-func iNeedToCreateMy_project() error {
-	projectName := "my_project"
+var projectName []string
+
+func iNeedToCreateMy_project(name string) error {
+	projectName = []string{name}
 	return nil
 }
 
-func iInvokeLlamaInitMy_project(projectName string) error {
+func iInvokeLlamaInitMy_project() error {
 	cmd.CreateProject(projectName)
 	return nil
 }
@@ -20,7 +24,20 @@ func aDirectoryWithAllNeededFilesWillBeCreated() error {
 }
 
 func FeatureContext(s *godog.Suite) {
-	s.Step(`^I need to create my_project$`, iNeedToCreateMy_project)
-	s.Step(`^I invoke llama init my_project$`, iInvokeLlamaInitMy_project)
+	s.Step(`^I need to create (\w+)$`, iNeedToCreateMy_project)
+	s.Step(`^I invoke llama init (\w+)$`, iInvokeLlamaInitMy_project)
 	s.Step(`^a directory with all needed files will be created$`, aDirectoryWithAllNeededFilesWillBeCreated)
+
+	s.AfterScenario(func(interface{}, error) {
+		//current_path, err := os.Getwd()
+		//if err != nil {
+		//fmt.Println(err)
+		//}
+		fmt.Println(projectName)
+		//err = os.RemoveAll(current_path + "/" + projectName[0])
+		//if err != nil {
+		//fmt.Println(err)
+		//}
+	})
+
 }
